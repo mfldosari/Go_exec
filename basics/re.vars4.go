1ac4ee5d5ed97ad7398462318f7cd28ba990a6f3
jax wood <jax.wood.m@gmail.com>
2025-05-10 20:31:48 +0300

    finished with last section of vars

---
+++ basics/re.vars4.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		name   string = "alex"
+		age    int    = 78
+		maried bool   = false
+	)
+
+	fmt.Println(name)
+	fmt.Println(age)
+
+	fmt.Println(maried)
+
+}
---
+++ basics/vars4.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		a int    = 0
+		b int    = 1
+		c string = "hello"
+	)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
